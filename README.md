16de43cfb9d331edec09dae02104cab61bc0c9be
Brian <brian.blaze@lntinfotech.com>
2021-03-04 10:11:51 +0530

    addition of the readme file and also the addition of the sample code for training

---
+++ README.md
@@ -0,0 +1,3 @@
+## Python Essential Training
+
+- Training the basics of the python based programming
---
+++ fundamentals/sample_05.py
@@ -0,0 +1,38 @@
+# Operators, Arithmetic, Assignment, Unary Minus, Relational, Logical, Boolean
+# Arithmetic Operators = +,-,*,/,%=Modulus,//=Quotient, **=Exponent
+x = 10
+y = 5
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+x = 12
+y = 5
+print(x / y)
+# Quotient
+print(x // y)
+# Modulus
+print(x % y)
+# Power
+print(x ** y)
+a = 2
+print(2 ** 10)
+
+# Assignment Operators
+x = 10
+y = 20
+z = x + y
+print(z)
+print(z + x)
+print(z + y)
+z += 5  # equivalent to: z = z+5
+print(z)
+z -= 5
+print(z)
+z *= 5
+print(z)
+z /= 5
+print(z)
+z %= 5
+print(z)
